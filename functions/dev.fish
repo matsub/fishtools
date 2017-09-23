8d0e9707b0ec04cf6dc306825acfb98704661353
matsub <matsub.rk@gmail.com>
2017-09-23 16:36:09 +0900

    fix assigning session name

--- functions/dev.fish
+++ functions/dev.fish
@@ -5,7 +5,7 @@ function dev -d "cd to a repository registered in ghq"
     # rename session if in tmux
     if test -n "$TMUX"
         set repo_name (basename $moveto)
-        set session_name (string replace -r "\." "-" -- $repo_name)
+        set session_name (string replace -ar "\." "-" -- $repo_name)
         tmux rename-session $session_name
     end
 end
